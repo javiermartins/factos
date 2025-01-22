6a9e44bdd18da4f77aa3285d04b7503005b9c2c8
javiermartins <javiimartinserrano@gmail.com>
2025-01-22 20:53:10 +0100

    Sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
+  site: 'https://factos.pages.dev',
+  integrations: [tailwind(), sitemap()],
   markdown: {
     remarkPlugins: [],
   },
